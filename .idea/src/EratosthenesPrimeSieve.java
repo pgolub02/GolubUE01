5ab94893e15a493e4af57e39c11cbccc2f079a1b
pgolub02 <GolubP190058@sus.htl-grieskirchen.at>
2021-09-21 05:46:04 +0200

    2.Schritt

---
+++ .idea/src/EratosthenesPrimeSieve.java
@@ -0,0 +1,19 @@
+public class EratosthenesPrimeSieve implements PrimeSieve{
+
+    private int runTill = -1;
+
+    public EratosthenesPrimeSieve(int runTill){
+         this.runTill = runTill;
+    }
+
+    @Override
+    public boolean isPrime(int p) {
+
+        return false;
+    }
+
+    @Override
+    public void printPrimes() {
+
+    }
+}
