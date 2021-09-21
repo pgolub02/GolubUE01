7fe422bc6eac1cd483d56cd97ec8e5f5fcb4e539
pgolub02 <GolubP190058@sus.htl-grieskirchen.at>
2021-09-21 09:08:04 +0200

    4.Schritt

--- .idea/src/PrimeSieve.java
+++
@@ -1,6 +0,0 @@
-public interface PrimeSieve {
-    //Ueberprueft ob eine Primzahl ist oder nicht.
-    public boolean isPrime(int p) ;
-    //Gibt alle Primzahlen bis zur O begrenzt auf der Konsole aus .
-    public void printPrimes();
-}
