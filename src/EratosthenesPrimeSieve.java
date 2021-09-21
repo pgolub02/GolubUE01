717f29627b8d0d2748960a620104e2e46deb47fa
pgolub02 <GolubP190058@sus.htl-grieskirchen.at>
2021-09-21 09:26:14 +0200

    3.Schritt

--- src/EratosthenesPrimeSieve.java
+++ src/EratosthenesPrimeSieve.java
@@ -1,12 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class EratosthenesPrimeSieve implements PrimeSieve{
 
-    public EratosthenesPrimeSieve(int ){
+    private int runTill = -1;
 
+    public EratosthenesPrimeSieve(int runTill){
+        this.runTill = runTill;
     }
+
     @Override
     public boolean isPrime(int p) {
+        List<Boolean> prime = new ArrayList<Boolean>();
 
-        return false;
+        for(int i = 0;i <= p;++i) {
+            prime.add(true);
+        }
+        for(int i = 2;i*i <= p;++i){
+            if(prime.get(i) == true){
+                for(int j= i*i;i <= p;j += i){
+                    prime.set(j, false);
+                }
+            }
+        }
+        return prime.get(p+1);
     }
 
     @Override
